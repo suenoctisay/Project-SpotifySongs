2dd78f4161008fd4104ef2a766f43177fb5badc9
Suelen Santos <suelen_santos2002@outlook.com>
2025-08-10 18:16:53 -0300

    refactor: ajustes e limpeza de arquivos na pasta app

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,9 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  templateUrl: './app.html'
 })
+
 export class App {
   protected readonly title = signal('SpotifySongs');
 }
