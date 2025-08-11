735aa454219e3b43e701c06fb7cf42a19c250fd1
Suelen Santos <suelen_santos2002@outlook.com>
2025-08-11 11:52:42 -0300

    fix: ajuste de rota principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const app_routes: Routes = [
   {
-    path: '', redirectTo: 'songs', pathMatch: 'full'
+    path: '', redirectTo: 'home', pathMatch: 'full'
   },
   {
     path: 'home',
