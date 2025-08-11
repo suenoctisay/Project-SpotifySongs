a59117c5069f194d2d333118411dabdfb59115ab
Suelen Santos <suelen_santos2002@outlook.com>
2025-08-11 10:00:18 -0300

    refactor: ajuste no nome do componente App para AppComponent e delete de modules

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html'
 })
 
-export class App {
+export class AppComponent {
   protected readonly title = signal('SpotifySongs');
 }
