f3427da31772c311d1a8f615dda3c8f04ebaa14a
Suelen Santos <suelen_santos2002@outlook.com>
2025-08-13 18:36:27 -0300

    style: criação e estilização do componente footer

--- src/app/pages/songs/songs.ts
+++ src/app/pages/songs/songs.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 
 import { HeaderCompoenent } from "../../core/main-layout/header/header";
+import { FooterComponent } from '../../core/main-layout/footer/footer';
 
 @Component({
   selector: 'app-songs',
   templateUrl: './songs.html',
   styleUrl: './songs.scss',
   imports: [
-    HeaderCompoenent
+    HeaderCompoenent,
+    FooterComponent
   ],
 })
 
