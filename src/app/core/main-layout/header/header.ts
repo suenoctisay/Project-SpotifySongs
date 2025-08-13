b16de1bcfd2f6975f3098b3166540de69ee36a33
Suelen Santos <suelen_santos2002@outlook.com>
2025-08-13 15:34:07 -0300

    style: criação e estilização dos componentes header e sidenav

--- src/app/core/main-layout/header/header.ts
+++ src/app/core/main-layout/header/header.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+// angular material components
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.html',
   styleUrl: './header.scss',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule
+  ],
 })
 
-export class HeaderCompoenent { }
+export class HeaderCompoenent {
+  showFiller = false;
+  isSidenavVisible: boolean = false;
+
+  toggleSidenav(): void {
+    this.isSidenavVisible = !this.isSidenavVisible;
+  }
+
+  redirectToHome() {
+    window.location.href = '/home';
+  }
+}
