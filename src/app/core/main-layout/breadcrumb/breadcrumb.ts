3b6b82c6e9c30c7617e8235933618c22bcef3476
Suelen Santos <suelen_santos2002@outlook.com>
2025-08-13 19:14:34 -0300

    style: criação e estilização do componente breadcrumb

---
+++ src/app/core/main-layout/breadcrumb/breadcrumb.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrumb',
+  templateUrl: './breadcrumb.html',
+  styleUrl: './breadcrumb.scss',
+  imports: [ ],
+})
+
+export class BreadcrumbComponent { }
--- src/app/pages/songs/songs.ts
+++ src/app/pages/songs/songs.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 
 import { HeaderCompoenent } from "../../core/main-layout/header/header";
+import { BreadcrumbComponent } from '../../core/main-layout/breadcrumb/breadcrumb';
 
 @Component({
   selector: 'app-songs',
   templateUrl: './songs.html',
   styleUrl: './songs.scss',
   imports: [
-    HeaderCompoenent
+    HeaderCompoenent,
+    BreadcrumbComponent
   ],
 })
 
